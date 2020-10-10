2e36798913561e385b8ffb86db0e0d90160151a7
MARTHA <martha_15_7@outlook.com>
2020-10-09 20:28:21 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace hangmanGame
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        App()
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ru-RU");
+            //System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en"); 
+        }
+    }
+}
